63bb0f2999206366b145ea32667ddbc850a994bc
Thor Prestbøen <thor.erling.prestboen@gmail.com>
2024-11-15 13:11:39 +0100

    Fixed issue with port

--- server.js
+++ server.js
@@ -21,7 +21,8 @@ initSocketIO(server);
 app.use('/', routes);
 
 // Start the server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
